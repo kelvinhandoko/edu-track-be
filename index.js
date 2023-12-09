230cd8c5de13135ee15d4009ca8a68c51d05a70f
kelvin handoko <kelvinhandoko03@gmail.com>
2023-12-09 21:48:23 +0700

    fix: router issue

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const profileRouter = require("./route/v1/profile.routes");
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const router = require("./route")
 
-const app = express();
-const port = process.env.PORT || 5050;
+const app = express()
+const port = process.env.PORT || 5050
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan("dev"));
-app.use("/api", profileRouter);
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(morgan("dev"))
+app.use("/api", router)
 
 app.listen(port, () => {
-  console.log(`⚡️ server is run on: http://localhost:${port}`);
-});
+    console.log(`⚡️ server is run on: http://localhost:${port}`)
+})
