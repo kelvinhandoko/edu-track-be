91ab2c558eca99ab1f2e46acfaa03aaf9c0c71fa
kelvin handoko <89412241+kelvinhandoko@users.noreply.github.com>
2023-12-11 19:43:17 +0700

    feat: firebase config (#3)

---
+++ middleware/authentication.js
@@ -0,0 +1,17 @@
+const firebase = require("../config/firebase")
+
+const authentication = async (req, res, next) => {
+    try {
+        const { authorization } = req.headers
+        if (!authorization) return res.status(401).json({ message: "please login" })
+        const access_token = authorization.split(" ")
+        if (access_token[0] !== "Bearer") return res.status(401).json("unAuthorized")
+        const decodedToken = await firebase.auth().verifyIdToken(access_token[1])
+        req.user = decodedToken
+        next()
+    } catch (error) {
+        res.status(403).send("Unauthorized")
+    }
+}
+
+module.exports = authentication
