3d4922339d69d24992a7b6ac11fead20c31154f8
kelvin handoko <kelvinhandoko03@gmail.com>
2023-12-11 19:41:13 +0700

    feat: firebase config

--- controller/ProfileController.js
+++ controller/ProfileController.js
@@ -1,95 +1,98 @@
-const BaseController = require("./BaseController");
+const BaseController = require("./BaseController")
 
 class ProfileController extends BaseController {
-  /**
-   * Create a new profile.
-   * @param {import('express').Request} req - The request object from Express.
-   * @param {import('express').Response} res - The response object from Express.
-   * @returns {Promise<import("@prisma/client").Profile>}
-   */
-  async create(req, res) {
-    try {
-      const { dob, fullname } = req.body;
-      const createProfile = await this.db.profile.create({
-        data: { dob, fullname },
-      });
-      return this.created(res, {
-        code: res.statusCode,
-        data: createProfile,
-        message: "profile created successfully",
-      });
-    } catch (error) {
-      return this.fail(res, error.message);
+    /**
+     * Create a new profile.
+     * @param {import('express').Request} req - The request object from Express.
+     * @param {import('express').Response} res - The response object from Express.
+     * @returns {Promise<import("@prisma/client").Profile>}
+     */
+    async create(req, res) {
+        try {
+            const { uid } = req.user
+            const { dob, fullname } = req.body
+            const findUser = await this.db.profile.findUnique({ where: { userId: uid } })
+            if (findUser) return this.conflict(res, "this user profile already created")
+            const createProfile = await this.db.profile.create({
+                data: { dob: new Date(), fullname, userId: uid },
+            })
+            return this.created(res, {
+                code: res.statusCode,
+                data: createProfile,
+                message: "profile created successfully",
+            })
+        } catch (error) {
+            return this.fail(res, error.message)
+        }
     }
-  }
 
-  /**
-   * Get profile details by ID.
-   * @param {import('express').Request} req - The request object from Express.
-   * @param {import('express').Response} res - The response object from Express.
-   * @returns {Promise<import("@prisma/client").Profile>}
-   */
-  async getDetail(req, res) {
-    try {
-      const { id } = req.params;
-      const result = await this.db.profile.findUnique({ where: { id } });
-  
-      if (!result) {
-        return this.notFound(res, "Profile not found.");
-      }
-  
-      return this.ok(res, {
-        code: res.statusCode,
-        data: result,
-        message: "Successfully retrieved profile details",
-      });
-    } catch (error) {
-      return this.fail(res, error.message);
-    }
-  }
+    /**
+     * Get profile details by ID.
+     * @param {import('express').Request} req - The request object from Express.
+     * @param {import('express').Response} res - The response object from Express.
+     * @returns {Promise<import("@prisma/client").Profile>}
+     */
+    async getDetail(req, res) {
+        try {
+            const { uid } = req.user
+            const result = await this.db.profile.findUnique({ where: { userId: uid } })
 
-  /**
-   * Update profile details by ID.
-   * @param {import('express').Request} req - The request object from Express.
-   * @param {import('express').Response} res - The response object from Express.
-   * @returns {Promise<import("@prisma/client").Profile>}
-   */
-  async update(req, res) {
-    try {
-      const { id } = req.params;
-      const { dob, fullname } = req.body;
-      const updatedProfile = await this.db.profile.update({
-        where: { id },
-        data: { dob, fullname },
-      });
-      return this.ok(res, {
-        code: res.statusCode,
-        data: updatedProfile,
-        message: "profile updated successfully",
-      });
-    } catch (error) {
-      return this.fail(res, error.message);
-    }
-  }
+            if (!result) {
+                return this.notFound(res, "Profile not found.")
+            }
 
-  /**
-   * Delete profile by ID.
-   * @param {import('express').Request} req - The request object from Express.
-   * @param {import('express').Response} res - The response object from Express.
-   * @returns {Promise<void>}
-   */
-  async delete(req, res) {
-    try {
-      const { id } = req.params;
-      await this.db.profile.delete({ where: { id } });
-      return this.ok(res, {
-        code: res.statusCode,
-        message: "profile deleted successfully",
-      });
-    } catch (error) {
-      return this.fail(res, error.message);
+            return this.ok(res, {
+                code: res.statusCode,
+                data: result,
+                message: "Successfully retrieved profile details",
+            })
+        } catch (error) {
+            return this.fail(res, error.message)
+        }
     }
-  }
+
+    /**
+     * Update profile details by ID.
+     * @param {import('express').Request} req - The request object from Express.
+     * @param {import('express').Response} res - The response object from Express.
+     * @returns {Promise<import("@prisma/client").Profile>}
+     */
+    async update(req, res) {
+        try {
+            const { uid } = req.user
+            const { dob, fullname } = req.body
+            const updatedProfile = await this.db.profile.update({
+                where: { userId: uid },
+                data: { dob, fullname },
+            })
+            return this.ok(res, {
+                code: res.statusCode,
+                data: updatedProfile,
+                message: "profile updated successfully",
+            })
+        } catch (error) {
+            return this.fail(res, error.message)
+        }
+    }
+
+    /**
+     * Delete profile by ID.
+     * @param {import('express').Request} req - The request object from Express.
+     * @param {import('express').Response} res - The response object from Express.
+     * @returns {Promise<void>}
+     */
+    async delete(req, res) {
+        try {
+            const { uid } = req.user
+            await this.db.profile.delete({ where: { userId: uid } })
+            return this.ok(res, {
+                code: res.statusCode,
+                message: "profile deleted successfully",
+            })
+        } catch (error) {
+            return this.fail(res, error.message)
+        }
+    }
 }
 
-module.exports = ProfileController;
+module.exports = ProfileController
--- route/v1/profile.routes.js
+++ route/v1/profile.routes.js
@@ -1,13 +1,13 @@
-const { Router } = require("express");
-const ProfileController = require("../../controller/ProfileController");
+const { Router } = require("express")
+const ProfileController = require("../../controller/ProfileController")
 
-const profileRouter = Router();
+const profileRouter = Router()
 
-const profileController = new ProfileController();
+const profileController = new ProfileController()
 
-profileRouter.post("/", profileController.create);
-profileRouter.get("/:id", profileController.getDetail);
-profileRouter.put("/:id", profileController.update);
-profileRouter.delete("/:id", profileController.delete);
+profileRouter.post("/", profileController.create)
+profileRouter.get("/", profileController.getDetail)
+profileRouter.put("/", profileController.update)
+profileRouter.delete("/", profileController.delete)
 
-module.exports = profileRouter;
+module.exports = profileRouter
