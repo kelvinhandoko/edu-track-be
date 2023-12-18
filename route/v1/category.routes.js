a1487c6cdc895bcab26804dc29399f6ebee50a0f
Jeksen <115960575+Limzen@users.noreply.github.com>
2023-12-18 21:33:55 +0700

    feat: menambahkan CRUD untuk category (#4)

---
+++ route/v1/category.routes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const CategoryController = require("../../controller/CategoryController");
+const authentication = require("../../middleware/authentication");
+
+const categoryRouter = Router();
+const categoryController = new CategoryController();
+
+categoryRouter.use(authentication); 
+
+categoryRouter.post("/", categoryController.create);
+categoryRouter.get("/", categoryController.getAll);
+categoryRouter.get("/:id", categoryController.getDetail);
+categoryRouter.put("/:id", categoryController.update);
+categoryRouter.delete("/:id", categoryController.delete);
+
+module.exports = categoryRouter;
--- route/v1/index.js
+++ route/v1/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express")
 const lecturerRouter = require("./lecturer.routes")
 const profileRouter = require("./profile.routes")
+const categoryRouter = require("./category.routes")
 const authentication = require("../../middleware/authentication")
 
 const v1Router = Router()
@@ -8,6 +9,7 @@ v1Router.use(authentication)
 
 v1Router.use("/lecturer", lecturerRouter)
 v1Router.use("/profile", profileRouter)
+v1Router.use("/category", categoryRouter)
 
 v1Router.get("/user", (req, res) => {
     const user = req.user
