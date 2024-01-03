8997129bc718a733542e99a2e90c4676c1ac8c54
kelvin handoko <kelvinhandoko03@gmail.com>
2024-01-03 22:05:18 +0700

    feat: add test on profile

---
+++ vitest.config.integration.js
@@ -0,0 +1,13 @@
+// vitest.config.integration.ts
+import { defineConfig } from "vitest/config"
+export default defineConfig({
+    test: {
+        include: ["tests/**/*.test.js", "!/tests"],
+        setupFiles: ["/tests/helpers/setup.js"],
+        threads: false,
+        coverage: {
+            reporter: ["html", "json"],
+            enabled: true,
+        },
+    },
+})
